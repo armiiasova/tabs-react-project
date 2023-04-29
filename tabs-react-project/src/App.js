a5b554859a2785a8d9d2975ffeb464adc05cfdb6
Cholpon Zhanybek uulu <armiiasova.pl@gmail.com>
2023-04-29 22:41:20 +0200

    First draft is completed

---
+++ tabs-react-project/src/App.js
@@ -0,0 +1,66 @@
+import { Component } from "react";
+import frameworks from "./data/frameworks";
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: frameworks,
+      titleOfTheButtonCliked: null,
+      description: "",
+      img: ""
+    };
+  }
+
+  saveTheButtonThatWasClicked = ({ title, description, img }) => {
+    this.setState({
+      titleOfTheButtonCliked: title,
+      description: description,
+      img: img
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h2> this is my app</h2>
+        <button
+          onClick={() => this.saveTheButtonThatWasClicked(this.state.data[0])}
+          className={
+            this.state.titleOfTheButtonCliked === this.state.data[0].title
+              ? "active"
+              : null
+          }
+        >
+          {this.state.data[0].title}
+        </button>
+        <button
+          onClick={() => this.saveTheButtonThatWasClicked(this.state.data[1])}
+          className={
+            this.state.titleOfTheButtonCliked === this.state.data[1].title
+              ? "active"
+              : null
+          }
+        >
+          {this.state.data[1].title}
+        </button>
+        <button
+          onClick={() => this.saveTheButtonThatWasClicked(this.state.data[2])}
+          className={
+            this.state.titleOfTheButtonCliked === this.state.data[2].title
+              ? "active"
+              : null
+          }
+        >
+          {this.state.data[2].title}
+        </button>
+        <p>{this.state.description}</p>
+        <img src={this.state.img} alt={this.state.titleOfTheButtonCliked} />
+      </div>
+    );
+  }
+}
+export default App;
+
+
